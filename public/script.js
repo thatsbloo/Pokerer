df76f25b5a4b8ab9a9dc2e9a822e426471367d15
Bloo <85105953+thatsbloo@users.noreply.github.com>
2026-07-07 04:58:16 +0300

    Styles & Transitions
    its soooooooooooo ugly i'll make code prettier in the future if i have time or willpower

--- public/script.js
+++ public/script.js
@@ -3,9 +3,64 @@ import { doc, setDoc } from "https://www.gstatic.com/firebasejs/12.15.0/firebase
 
 console.log("loaded!!");
 async function test() {
-  console.log("test");
-  const docRef = doc(db, "test", "1");
-  await setDoc(docRef, { name: "John Doe" });
+    console.log("test");
+    const docRef = doc(db, "test", "1");
+    await setDoc(docRef, { name: "John Doe" });
 }
 
-document.getElementById("hostlobby").addEventListener("click", test);
+function animateButton(button) {
+    var style = window.getComputedStyle(button);
+    console.log(screen.width);
+    var margin = parseInt(style.getPropertyValue("margin-left"));
+    console.log(margin);
+    console.log(style.getPropertyValue("box-sizing"));
+    button.style.width = window.innerWidth - margin*2 + "px";
+    button.style.height = "100px";
+    button.style.color = "rgba(244, 255, 194, 0)";
+}
+
+document.getElementById("hostlobby").addEventListener("click", () => {
+    animateButton(document.getElementById("hostlobby"));
+    document.getElementById("joinlobby").hidden = true;
+    document.getElementById("hostlobbybr").hidden = true;
+    test();
+});
+
+
+document.getElementById("hostlobby").addEventListener("transitionend", (event) => {
+    if (event.propertyName !== "width") return;
+
+    var promptdiv = document.getElementById("promptdiv");
+    var btnstyle = window.getComputedStyle(event.target);
+
+    promptdiv.style.width = btnstyle.getPropertyValue("width");
+    promptdiv.style.height = btnstyle.getPropertyValue("height");
+    promptdiv.style.padding = btnstyle.getPropertyValue("padding");
+    promptdiv.style.display = "inline-block";
+    event.target.hidden = true;
+    promptdiv.hidden = false;
+    
+});
+
+document.getElementById("joinlobby").addEventListener("click", () => {
+    document.getElementById("hostlobby").hidden = true;
+    document.getElementById("hostlobbybr").hidden = true;
+    animateButton(document.getElementById("joinlobby"));
+    
+});
+
+document.getElementById("joinlobby").addEventListener("transitionend", (event) => {
+    if (event.propertyName !== "width") return;
+
+    var promptdiv = document.getElementById("promptdiv");
+    var btnstyle = window.getComputedStyle(event.target);
+
+    promptdiv.style.width = btnstyle.getPropertyValue("width");
+    promptdiv.style.height = btnstyle.getPropertyValue("height");
+    promptdiv.style.padding = btnstyle.getPropertyValue("padding");
+    promptdiv.style.display = "inline-block";
+    event.target.hidden = true;
+    promptdiv.hidden = false;
+
+});
+
